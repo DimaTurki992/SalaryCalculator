862eac16eb9980f2500095cb3ed8f4154fdbba0b
Dima <turki@capella.io>
2022-08-03 19:47:05 +0300

    Update Employee.cs
    added Id to Employee model

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -3,6 +3,7 @@ namespace EmployeesSalaries.Models
 {
     public class Employee
     {
+        public int Id { get; set; }
         public double BasicSalary { get; set; }
         public virtual double GetTotalSalary()
         {
